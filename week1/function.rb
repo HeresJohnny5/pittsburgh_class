2c77d7422fd2314cf9fde035b5ade429b4294086
John Erickson <ericksonjohn08@hotmail.com>
2017-01-15 21:08:57 -0500

    Add 3 different tests resulting in Mined.

--- week1/function.rb
+++ week1/function.rb
@@ -1,7 +1,9 @@
 class Algorithm 
 
     def fizz_buzz(num)
-       num
+       if num % 3 == 0 
+        "Mined"
+       end
     end
-    
+
 end
--- week1/test.rb
+++ week1/test.rb
@@ -2,8 +2,18 @@ require_relative 'function.rb'
 require 'minitest/autorun'
 
 class FizzBuzz < Minitest::Test
-  def test_returns_number
+  def test_3_returns_Mined
     algorithm = Algorithm.new
-    assert_equal(3, algorithm.fizz_buzz(3))
+    assert_equal("Mined", algorithm.fizz_buzz(3))
+  end
+
+  def test_6_returns_Mined
+    algorithm = Algorithm.new
+    assert_equal("Mined", algorithm.fizz_buzz(6))
+  end
+
+  def test_129_returns_Mined
+    algorithm = Algorithm.new
+    assert_equal("Mined", algorithm.fizz_buzz(129))
   end
 end
