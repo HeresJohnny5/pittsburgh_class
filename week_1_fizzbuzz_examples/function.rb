6923e54128b21a53e9dcee89dd6d802571fc186f
John Erickson <ericksonjohn08@hotmail.com>
2017-01-20 00:47:04 -0500

    Add additional functionality to if conditional. Add tests for both numbers divisible by both 3 and 5 and numbers divisible by 3.

--- week_1_fizzbuzz_examples/function.rb
+++ week_1_fizzbuzz_examples/function.rb
@@ -5,6 +5,8 @@ def numbered_array(up_to_number)
 	while(i <= up_to_number)
 		if (i % 3 == 0 && i % 5 == 0)
 			new_array.push("Mined Minds")
+		elsif (i % 3 == 0)
+			new_array.push("Mined")
 		else
 			new_array.push(i)
 		end
@@ -13,3 +15,5 @@ def numbered_array(up_to_number)
 	
 	new_array
 end
+
+#puts numbered_array(100).inspect
--- week_1_fizzbuzz_examples/test.rb
+++ week_1_fizzbuzz_examples/test.rb
@@ -12,8 +12,23 @@ class TestMinedMindsKata < Minitest::Test
 		assert_equal("Mined Minds", numbered_array(up_to_number)[14])
 	end
 	
-	def test_15_returns_Mined_Minds
+	def test_30_returns_Mined_Minds
 		up_to_number = 100
 		assert_equal("Mined Minds", numbered_array(up_to_number)[29])
+	end
+	
+	def test_3_returns_Mined
+		up_to_number = 100
+		assert_equal("Mined", numbered_array(up_to_number)[2])
+	end
+	
+	def test_6_returns_Mined
+		up_to_number = 100
+		assert_equal("Mined", numbered_array(up_to_number)[5])
+	end
+	
+	def test_99_returns_Mined
+		up_to_number = 100
+		assert_equal("Mined", numbered_array(up_to_number)[98])
 	end
 end
