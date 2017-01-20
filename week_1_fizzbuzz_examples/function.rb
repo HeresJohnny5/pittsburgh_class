0cdb0ccf298f08aa399fabab0d186d3dea7d6853
John Erickson <ericksonjohn08@hotmail.com>
2017-01-20 00:56:38 -0500

    Add until loop kata.

--- week_1_fizzbuzz_examples/function.rb
+++ week_1_fizzbuzz_examples/function.rb
@@ -1,8 +1,28 @@
+#def numbered_array(up_to_number)
+#	new_array = []
+#	i = 1
+#	
+#	while(i <= up_to_number)
+#		if (i % 3 == 0 && i % 5 == 0)
+#			new_array.push("Mined Minds")
+#		elsif (i % 3 == 0)
+#			new_array.push("Mined")
+#		elsif (i % 5 == 0)
+#			new_array.push("Minds")
+#		else
+#			new_array.push(i)
+#		end
+#		i += 1
+#	end
+#	
+#	new_array
+#end
+
 def numbered_array(up_to_number)
 	new_array = []
 	i = 1
 	
-	while(i <= up_to_number)
+	until(i > up_to_number)
 		if (i % 3 == 0 && i % 5 == 0)
 			new_array.push("Mined Minds")
 		elsif (i % 3 == 0)
