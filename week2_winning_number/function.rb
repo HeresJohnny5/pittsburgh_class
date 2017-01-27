bcf6bd51814cec2ffde3000b63808b2476dd5ae5
John Erickson <ericksonjohn08@hotmail.com>
2017-01-27 01:29:04 -0500

    Add winning_number_each? method to function.rb. Add 2 new tests to test.rb.

--- week2_winning_number/function.rb
+++ week2_winning_number/function.rb
@@ -1,3 +1,15 @@
-def winning_number?(my_ticket, winning_ticket)
-  winning_ticket.include?(my_ticket)
+def winning_number_include?(my_ticket, winning_tickets)
+  winning_tickets.include?(my_ticket)
+end
+
+def winning_number_each?(my_ticket, winning_tickets)
+  winning_number = false
+
+  winning_tickets.each do |winning_ticket|
+    if my_ticket == winning_ticket
+      return true
+    end
+  end
+
+  winning_number
 end
--- week2_winning_number/test.rb
+++ week2_winning_number/test.rb
@@ -4,25 +4,37 @@ require_relative 'function.rb'
 class TestWinningNumber < Minitest::Test
   def test_ticket_5423_returns_true
     my_ticket = "5423"
-    winning_ticket = ["5423"]
-    assert_equal(true, winning_number?(my_ticket, winning_ticket))
+    winning_tickets = ["5423"]
+    assert_equal(true, winning_number_include?(my_ticket, winning_tickets))
   end
 
   def test_ticket_4555_returns_true
     my_ticket = "4555"
-    winning_ticket = ["4555"]
-    assert_equal(true, winning_number?(my_ticket, winning_ticket))
+    winning_tickets = ["4555", "5143", "1232"]
+    assert_equal(true, winning_number_include?(my_ticket, winning_tickets))
   end
 
   def test_ticket_2198_returns_false
     my_ticket = "2198"
-    winning_ticket = ["8927"]
-    assert_equal(false, winning_number?(my_ticket, winning_ticket))
+    winning_tickets = ["8927"]
+    assert_equal(false, winning_number_include?(my_ticket, winning_tickets))
   end
 
   def test_ticket_1298_returns_false
     my_ticket = "1298"
-    winning_ticket = ["5241"]
-    assert_equal(false, winning_number?(my_ticket, winning_ticket))
+    winning_tickets = ["5241", "3836", "1392"]
+    assert_equal(false, winning_number_include?(my_ticket, winning_tickets))
+  end
+
+  def test_ticket_1393_returns_true
+    my_ticket = "1393"
+    winning_tickets = ["5231", "3836", "1393"]
+    assert_equal(true, winning_number_each?(my_ticket, winning_tickets))
+  end
+
+  def test_ticket_5142_returns_false
+    my_ticket = "5142"
+    winning_tickets = ["5231", "3836", "1393", "7263", "5143", "7153"]
+    assert_equal(false, winning_number_each?(my_ticket, winning_tickets))
   end
 end
