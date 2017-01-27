c966b8f1ecf5dd99a812fb26cf2ffbc950c1bf7e
John Erickson <ericksonjohn08@hotmail.com>
2017-01-27 09:06:43 -0500

    Add close_but_no_cigar function to function.rb. Add 2 tests to test.rb.

--- week2_close_but_no_cigar/function.rb
+++ week2_close_but_no_cigar/function.rb
@@ -13,3 +13,16 @@ def off_by_one?(num1, num2)
 
   counter == num1.length - 1
 end
+
+# Write a function that takes a number (our ticket number) and an array of numbers (the winning numbers from all the draws). To start with, just restrict to all 4 digit numbers. Having the numbers as strings may make it easier to solve. The function should return an array of all the winning numbers that are one digit off from our ticket number. eg our_number = '1234' and winning_numbers = ['9999', '5678', '1235', '1134', '1344'] should return ['1235', '1134']
+
+def close_but_no_cigar(my_ticket, winning_numbers_array)
+  matches = []
+
+  winning_numbers_array.each do |winning_number|
+    if off_by_one?(my_ticket, winning_number)
+      matches.push(winning_number)
+    end
+  end
+  matches
+end
--- week2_close_but_no_cigar/test.rb
+++ week2_close_but_no_cigar/test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'function.rb'
 
-class CloseButNoCigar < Minitest::Test
+class OffByOne < Minitest::Test
   def test_1234_compared_to_1235_returns_true
     num1 = "1234"
     num2 = "1235"
@@ -36,5 +36,22 @@ class CloseButNoCigar < Minitest::Test
     num1 = "4526"
     num2 = "4422"
     assert_equal(false, off_by_one?(num1, num2))
+  end
+end
+
+# *******************************************
+# **************** NEW CLASS ****************
+# *******************************************
+class CloseByNoCigar < Minitest::Test
+  def test_1234_returns_array_with_2_indices
+    my_ticket = "1234"
+    winning_numbers_array = ["9999", "5678", "1235", "1134", "1344"]
+    assert_equal(["1235", "1134"], close_but_no_cigar(my_ticket, winning_numbers_array))
+  end
+
+  def test_5627_returns_empty_array
+    my_ticket = "5627"
+    winning_numbers_array = ["9999", "5678", "1235", "1134", "1344"]
+    assert_equal([], close_but_no_cigar(my_ticket, winning_numbers_array))
   end
 end
