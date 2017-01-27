d58e0a15e7dbb9b5ea6053dd07c58c5f9a945dc5
John Erickson <ericksonjohn08@hotmail.com>
2017-01-27 00:38:18 -0500

    Add basic_subtraction method to function.rb and first test.

--- week2_arithmetic/function.rb
+++ week2_arithmetic/function.rb
@@ -1,3 +1,7 @@
 def basic_addition(num1, num2)
   num1 + num2
 end
+
+def basic_subtraction(num1, num2)
+  num1 - num2
+end
--- week2_arithmetic/test.rb
+++ week2_arithmetic/test.rb
@@ -5,4 +5,18 @@ class TestAddition < Minitest::Test
   def test_1_plus_0_equals_1
     assert_equal(1, basic_addition(1, 0))
   end
+
+  def test_55_plus_310_equals_365
+    assert_equal(365, basic_addition(55, 310))
+  end
+
+  def test_10_plus_15_equals_25
+    assert_equal(25, basic_addition(10, 15))
+  end
+end
+
+class TestSubtraction < Minitest::Test
+  def test_1_minus_0_equals_1
+    assert_equal(1, basic_subtraction(1, 0))
+  end
 end
