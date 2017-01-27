79757bf0d46439c4c64eaf94589ddd2896ee5139
John Erickson <ericksonjohn08@hotmail.com>
2017-01-27 08:49:28 -0500

    Add 3 additional tests to test.rb.

--- week2_close_but_no_cigar/test.rb
+++ week2_close_but_no_cigar/test.rb
@@ -19,4 +19,22 @@ class CloseButNoCigar < Minitest::Test
     num2 = "2435"
     assert_equal(false, off_by_one?(num1, num2))
   end
+
+  def test_8725_compared_to_8825_returns_true
+    num1 = "8725"
+    num2 = "8825"
+    assert_equal(true, off_by_one?(num1, num2))
+  end
+
+  def test_6253_compared_to_6715_returns_false
+    num1 = "6253"
+    num2 = "6715"
+    assert_equal(false, off_by_one?(num1, num2))
+  end
+
+  def test_4526_compared_to_4422_returns_false
+    num1 = "4526"
+    num2 = "4422"
+    assert_equal(false, off_by_one?(num1, num2))
+  end
 end
