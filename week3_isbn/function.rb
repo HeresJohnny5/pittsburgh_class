117b7c14425945879b5eb40352ff8af408573e94
John Erickson <ericksonjohn08@hotmail.com>
2017-01-31 15:04:01 -0500

    Add remove_disallowed_characters_from_isbn function to function.rb. Add 4 new tests to test.rb.

--- week3_isbn/function.rb
+++ week3_isbn/function.rb
@@ -1,7 +1,19 @@
-def valid_isbn_length?(num)
-  if num.length == 10 || num.length == 13
+def valid_isbn_length?(string_num)
+  #takes in string and returns boolean
+  if string_num.length == 10 || string_num.length == 13
     true
   else
     false
   end
+end
+
+def remove_disallowed_characters_from_isbn(string_num)
+  #takes in a string and returns a string without disallowed characters
+  disallowed_characters = [ " ", "-", "\n", "\""]
+
+  disallowed_characters.each do |characters|
+      string_num.delete! characters 
+  end 
+     
+  string_num
 end
--- week3_isbn/test.rb
+++ week3_isbn/test.rb
@@ -3,22 +3,44 @@ require_relative 'function.rb'
 
 class TestValidIsbnLength < Minitest::Test
   def test_empty_string_returns_false
-    num = ""
-    assert_equal(false, valid_isbn_length?(""))
+    string_num = ""
+    assert_equal(false, valid_isbn_length?(string_num))
   end
 
   def test_10_digit_string_returns_true()
-    num = "0471958697"
-    assert_equal(true, valid_isbn_length?("0471958697"))
+    string_num = "0471958697"
+    assert_equal(true, valid_isbn_length?(string_num))
   end
 
   def test_5_digit_string_returns_false()
-    num = "04719"
-    assert_equal(false, valid_isbn_length?("04719"))
+    string_num = "04719"
+    assert_equal(false, valid_isbn_length?(string_num))
   end
 
   def test_13_digit_string_returns_true()
-    num = "9780470059029"
-    assert_equal(true, valid_isbn_length?("9780470059029"))
+    string_num = "9780470059029"
+    assert_equal(true, valid_isbn_length?(string_num))
+  end
+end
+
+class TestDisallowedCharactersFunction < Minitest::Test
+  def test_to_remove_extra_characters_1234567890
+    string_num = "12 3-4567-89 0"
+    assert_equal("1234567890", remove_disallowed_characters_from_isbn(string_num))
+  end
+
+  def test_to_remove_extra_characters_5434567890
+    string_num = "5434567890\n"
+    assert_equal("5434567890", remove_disallowed_characters_from_isbn(string_num))
+  end
+
+  def test_to_remove_extra_characters_1235637890
+    string_num = "123 563-7890\n"
+    assert_equal("1235637890", remove_disallowed_characters_from_isbn(string_num))
+  end
+
+  def test_to_remove_extra_characters_5624567890
+    string_num = "562456\"7890"
+    assert_equal("5624567890", remove_disallowed_characters_from_isbn(string_num))
   end
 end
