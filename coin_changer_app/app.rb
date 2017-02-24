f13e5ed2d0c3b21fbf8db343c5838c5bcb230afe
John Erickson <ericksonjohn08@hotmail.com>
2017-02-24 08:33:11 -0500

    Add get_money.erb to view directory

--- coin_changer_app/app.rb
+++ coin_changer_app/app.rb
@@ -1,6 +1,14 @@
 require_relative 'function.rb'
 require 'sinatra'
 
+enable :sessions
+
 get '/' do
-  erb :name
+  erb :get_name
+end
+
+post '/get_name' do
+  session[:user_name] = params[:user_name]
+
+  erb :get_money, :locals => { :user_name => session[:user_name] }
 end
