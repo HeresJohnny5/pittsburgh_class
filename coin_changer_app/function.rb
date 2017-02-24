dfc0978d76861439a92738d3f93843a4539ab8fe
John Erickson <ericksonjohn08@hotmail.com>
2017-02-24 00:34:23 -0500

    Add dime conditional to coin_changer method. Add additional tests to test.rb

--- coin_changer_app/function.rb
+++ coin_changer_app/function.rb
@@ -2,7 +2,10 @@ def coin_changer(coin)
   coin_hash = { :penny => 0, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }
 
   while (coin > 0)
-    if coin >= 5
+    if coin >= 10
+      coin_hash[:dime] += 1
+      coin = coin - 10
+    elsif coin >= 5
       coin_hash[:nickel] += 1
       coin = coin - 5
     elsif coin > 0 && coin < 5
@@ -14,4 +17,4 @@ def coin_changer(coin)
   coin_hash
 end
 
-# puts coin_changer(11)
+puts coin_changer(20)
--- coin_changer_app/test.rb
+++ coin_changer_app/test.rb
@@ -9,12 +9,23 @@ class TestCoinChangerKata < Minitest::Test
     assert_equal({ :penny => 4, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(4))
   end
 
-  def test_responds_to_currency_hash_with_currency_greater_than_5_less_than_or_equal_to_10
+  def test_responds_to_currency_hash_with_currency_greater_than_5_less_than_10
     assert_equal({ :penny => 0, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(5))
     assert_equal({ :penny => 1, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(6))
     assert_equal({ :penny => 2, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(7))
     assert_equal({ :penny => 3, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(8))
     assert_equal({ :penny => 4, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(9))
-    assert_equal({ :penny => 0, :nickel => 2, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(10))
+  end
+
+  def test_responds_to_currency_hash_with_currency_greater_than_5_less_than_25
+    assert_equal({ :penny => 0, :nickel => 0, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(10))
+    assert_equal({ :penny => 1, :nickel => 0, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(11))
+    assert_equal({ :penny => 2, :nickel => 0, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(12))
+    assert_equal({ :penny => 3, :nickel => 0, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(13))
+    assert_equal({ :penny => 4, :nickel => 0, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(14))
+    assert_equal({ :penny => 0, :nickel => 1, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(15))
+    assert_equal({ :penny => 3, :nickel => 1, :dime => 1, :quarter => 0, :dollar => 0 }, coin_changer(18))
+    assert_equal({ :penny => 0, :nickel => 0, :dime => 2, :quarter => 0, :dollar => 0 }, coin_changer(20))
+    assert_equal({ :penny => 4, :nickel => 0, :dime => 2, :quarter => 0, :dollar => 0 }, coin_changer(24))
   end
 end
