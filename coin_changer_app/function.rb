0f263358502d4697f7c8ec9233b9873e822335f4
John Erickson <ericksonjohn08@hotmail.com>
2017-02-23 23:51:14 -0500

    Adjust coin_changer method. Apply parameter variable rather than hard code hash value. 4 tests passing

--- coin_changer_app/function.rb
+++ coin_changer_app/function.rb
@@ -2,8 +2,10 @@ def coin_changer(coin)
   coin_hash = { :penny => 0, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }
 
   if coin > 0 && coin < 5
-    coin_hash[:penny] = 1
+    coin_hash[:penny] = coin
   end
 
   coin_hash
 end
+
+puts coin_changer(2)
--- coin_changer_app/test.rb
+++ coin_changer_app/test.rb
@@ -2,7 +2,10 @@ require_relative 'function.rb'
 require 'minitest/autorun'
 
 class TestCoinChangerKata < Minitest::Test
-  def test_responds_to_currency_hash_adding_one_to_key_penny
+  def test_responds_to_currency_hash_with_currency_less_than_5
     assert_equal({ :penny => 1, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(1))
+    assert_equal({ :penny => 2, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(2))
+    assert_equal({ :penny => 3, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(3))
+    assert_equal({ :penny => 4, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(4))
   end
 end
