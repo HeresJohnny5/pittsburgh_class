82965e486a7efb7722433c1a39ee2aefa2ce3564
John Erickson <ericksonjohn08@hotmail.com>
2017-02-24 00:18:16 -0500

    Add while conditional with nested if, elsif conditional. Add additional tests

--- coin_changer_app/function.rb
+++ coin_changer_app/function.rb
@@ -1,11 +1,17 @@
 def coin_changer(coin)
   coin_hash = { :penny => 0, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }
 
-  if coin > 0 && coin < 5
-    coin_hash[:penny] = coin
+  while (coin > 0)
+    if coin >= 5
+      coin_hash[:nickel] = coin - (coin - 1)
+      coin = coin - 5
+    elsif coin > 0 && coin < 5
+      coin_hash[:penny] = coin
+      coin = coin - coin
+    end
   end
 
   coin_hash
 end
 
-puts coin_changer(2)
+puts coin_changer(10)
--- coin_changer_app/test.rb
+++ coin_changer_app/test.rb
@@ -8,4 +8,12 @@ class TestCoinChangerKata < Minitest::Test
     assert_equal({ :penny => 3, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(3))
     assert_equal({ :penny => 4, :nickel => 0, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(4))
   end
+
+  def test_responds_to_currency_hash_with_currency_greater_than_5_less_than_10
+    assert_equal({ :penny => 0, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(5))
+    assert_equal({ :penny => 1, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(6))
+    assert_equal({ :penny => 2, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(7))
+    assert_equal({ :penny => 3, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(8))
+    assert_equal({ :penny => 4, :nickel => 1, :dime => 0, :quarter => 0, :dollar => 0 }, coin_changer(9))
+  end
 end
