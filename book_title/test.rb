7880945f4d3efbda525b815c79a429f51afaafb7
John Erickson <ericksonjohn08@hotmail.com>
2017-03-11 13:41:58 -0500

    Add new test to test.rb

--- book_title/test.rb
+++ book_title/test.rb
@@ -2,8 +2,13 @@ require 'minitest/autorun'
 require_relative 'function.rb'
 
 class TestBookTitle < Minitest::Test
-  def test_responds_to_class_returns_new_array
+  def test_responds_to_class_returns_new_array_Lord_of_the_Rings
     book_title = BookTitleKata.new("lORd OF The riNGS")
     assert_equal(["Lord", "of", "the", "Rings"], book_title.updated_book_array)
+  end
+
+  def test_responds_to_class_returns_new_array_the_Blair_Witch_Project
+    book_title = BookTitleKata.new("THE BLAIR WITCH PROJECT")
+    assert_equal(["the", "Blair", "Witch", "Project"], book_title.updated_book_array)
   end
 end
